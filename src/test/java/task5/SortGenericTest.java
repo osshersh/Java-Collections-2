08a50f3f11dcc483a9600a9049b144d8450e2ad7
osshersh <olegshershnov@gmail.com>
2022-12-30 22:33:47 +0100

    Korekta testow dla task5

--- src/test/java/task5/SortGenericTest.java
+++ src/test/java/task5/SortGenericTest.java
@@ -2,9 +2,7 @@ package task5;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortGenericTest {
@@ -17,34 +15,33 @@ class SortGenericTest {
     @BeforeEach
     void init() {
         sortGeneric = new SortGeneric();
-        numbers = new Integer[]{4, 333, 44};
-        strings = new String[]{"winter", "autumn", "spring"};
-        characters = new Character[]{'w', 's', 'a'};
-        doubles = new Double[]{55.45, 45.09, 45.45};
-
-        sortGeneric.sortGenericArray(numbers);
-        sortGeneric.sortGenericArray(strings);
-        sortGeneric.sortGenericArray(characters);
-        sortGeneric.sortGenericArray(doubles);
     }
 
     @Test
     void shouldSortStringType() {
+        strings = new String[]{"winter", "autumn", "spring"};
+        sortGeneric.sortGenericArray(strings);
         assertEquals("[autumn, spring, winter]", Arrays.toString(strings));
     }
 
     @Test
     void shouldSortIntegerType() {
+        numbers = new Integer[]{4, 333, 44};
+        sortGeneric.sortGenericArray(numbers);
         assertEquals("[4, 44, 333]", Arrays.toString(numbers));
     }
 
     @Test
     void shouldSortCharacterType() {
+        characters = new Character[]{'w', 's', 'a'};
+        sortGeneric.sortGenericArray(characters);
         assertEquals("[a, s, w]", Arrays.toString(characters));
     }
 
     @Test
     void shouldSortDoubleType() {
+        doubles = new Double[]{55.45, 45.09, 45.45};
+        sortGeneric.sortGenericArray(doubles);
         assertEquals("[45.09, 45.45, 55.45]", Arrays.toString(doubles));
     }
 }
